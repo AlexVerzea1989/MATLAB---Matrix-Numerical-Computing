189f8f3985cc98bef2b41ac427e65d99daa9d580
AlexVerzea1989 <alexandru.verzea@sympatico.ca>
2015-04-08 13:45:01 -0400

    Create modifiedGramSchmidt.m

---
+++ modifiedGramSchmidt.m
@@ -0,0 +1,24 @@
+% ALEX VERZEA
+% 260324472
+% COMP 540 Assignment 4, #5.
+%
+% modifiedGramSchmidt.m    Modified Gram-Schmidt Orthogonalization
+%
+% input: A is an m x n matrix (full column rank)
+%
+% output: Q is an m x n matrix
+%         R is an n x n matrix
+%
+function [Q1, R] = modifiedGramSchmidt(A)
+[m, n] = size(A);
+Q1 = zeros(m, n);
+R = zeros(n,n);
+for k = 1:n,
+    a = A(:,k);
+    for i = 1:k-1
+        R(i,k) = Q1(:,i)' * A(:,k);
+        a = a - R(i,k) * Q1(:,i);
+    end;
+    R(k,k) = norm(a);
+    Q1(:,k) = a / R(k,k);
+end;
